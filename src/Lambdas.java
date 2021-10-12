28e77864fc07b7cd38015ca2aaae4c6bd1483a8b
SNException <thexsalkin@gmail.com>
2021-10-12 13:02:26 +0200

    Lambdas ex versions

--- src/Lambdas.java
+++ src/Lambdas.java
@@ -109,4 +109,90 @@ public final class Lambdas {
                            final Param7 p7, final Param8 p8, final Param9 p9,
                            final Param10 p10);
     }
+
+    @FunctionalInterface
+    public interface NullaryEx<Result> {
+
+        public Result call() throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface UnaryEx<Result, Param1> {
+
+         public Result call(final Param1 p1) throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface BinaryEx<Result, Param1, Param2> {
+
+        public Result call(final Param1 p1, final Param2 p2) throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface TernaryEx<Result, Param1, Param2, Param3> {
+
+        public Result call(final Param1 p1, final Param2 p2, final Param3 p3)
+                           throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface QuaternaryEx<Result, Param1, Param2, Param3, Param4> {
+        public Result call(final Param1 p1, final Param2 p2, final Param3 p3,
+                           final Param4 p4) throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface QuinaryEx<Result, Param1, Param2, Param3, Param4,
+                               Param5> {
+
+        public Result call(final Param1 p1, final Param2 p2, final Param3 p3,
+                           final Param4 p4, final Param5 p5) throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface SenaryEx<Result, Param1, Param2, Param3, Param4, Param5,
+                            Param6> {
+
+        public Result callEx(final Param1 p1, final Param2 p2, final Param3 p3,
+                           final Param4 p4, final Param5 p5, final Param6 p6)
+                           throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface SeptenaryEx<Result, Param1, Param2, Param3, Param4,
+                               Param5, Param6, Param7> {
+
+        public Result call(final Param1 p1, final Param2 p2, final Param3 p3,
+                           final Param4 p4, final Param5 p5, final Param6 p6,
+                           final Param7 p7) throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface OctonaryEx<Result, Param1, Param2, Param3, Param4, Param5,
+                              Param6, Param7, Param8> {
+
+        public Result call(final Param1 p1, final Param2 p2, final Param3 p3,
+                           final Param4 p4, final Param5 p5, final Param6 p6,
+                           final Param7 p7, final Param8 p8) throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface NovenaryEx<Result, Param1, Param2, Param3, Param4, Param5,
+                              Param6, Param7, Param8, Param9> {
+
+        public Result call(final Param1 p1, final Param2 p2, final Param3 p3,
+                           final Param4 p4, final Param5 p5, final Param6 p6,
+                           final Param7 p7, final Param8 p8, final Param9 p9)
+                           throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface DenaryEx<Result, Param1, Param2, Param3, Param4, Param5,
+                            Param6, Param7, Param8, Param9, Param10> {
+
+        public Result call(final Param1 p1, final Param2 p2, final Param3 p3,
+                           final Param4 p4, final Param5 p5, final Param6 p6,
+                           final Param7 p7, final Param8 p8, final Param9 p9,
+                           final Param10 p10) throws Exception;
+    }
 }
